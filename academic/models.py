ba2b2b2541516b0137abbbdf5d4dcde838fffe4b
MacMayambala <mayambala.mark@saozirobwe.co.ug>
2026-03-27 15:49:37 +0300

    Update models.py

--- academic/models.py
+++ academic/models.py
@@ -108,6 +108,7 @@ from django.db import models
 from django.conf import settings
 
 class Teacher(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='teachers')
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacher_profile')
     staff_id = models.CharField(max_length=20, unique=True)
     
