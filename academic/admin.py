e99cd460090b17a8a99fa9fc50c3f3a969d1e808
MacMayambala <mayambala.mark@saozirobwe.co.ug>
2026-03-27 17:02:53 +0300

    Update admin.py

--- academic/admin.py
+++ academic/admin.py
@@ -42,17 +42,26 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Teacher
 
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Teacher
+
+from django.utils.html import format_html
+
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    # What columns to show in the list view
-    list_display = ('display_photo', 'full_name', 'staff_id', 'phone', 'assigned_classes')
-    list_filter = ('classrooms', 'subjects')
-    search_fields = ('user__first_name', 'user__last_name', 'staff_id')
+    # Added 'school' to the list display for quick identification
+    list_display = ('display_photo', 'full_name', 'school', 'staff_id', 'phone', 'assigned_classes')
     
-    # Organizing the "Add/Edit" page
+    # Added 'school' to filters so you can view teachers per institution
+    list_filter = ('school', 'classrooms', 'subjects')
+    
+    search_fields = ('user__first_name', 'user__last_name', 'staff_id', 'school__name')
+    
     fieldsets = (
         ('Personal Identity', {
-            'fields': ('user', 'staff_id', 'profile_photo', 'phone')
+            # Included 'school' here so it's assigned during creation
+            'fields': ('user', 'school', 'staff_id', 'profile_photo', 'phone')
         }),
         ('Academic Responsibility', {
             'fields': ('classrooms', 'subjects'),
@@ -60,19 +69,16 @@ class TeacherAdmin(admin.ModelAdmin):
         }),
     )
 
-    # Helper to show full name in list
     def full_name(self, obj):
         return obj.user.get_full_name()
     full_name.short_description = 'Teacher Name'
 
-    # Helper to show photo thumbnail in list
     def display_photo(self, obj):
         if obj.profile_photo:
             return format_html('<img src="{}" style="width: 40px; height: 40px; border-radius: 50%; object-fit: cover;" />', obj.profile_photo.url)
         return format_html('<div style="width: 40px; height: 40px; border-radius: 50%; background: #eee; display: flex; align-items: center; justify-content: center; font-size: 10px; color: #999;">No Image</div>')
     display_photo.short_description = 'Photo'
 
-    # Helper to list assigned classes in the table
     def assigned_classes(self, obj):
         return ", ".join([c.name for c in obj.classrooms.all()])
     assigned_classes.short_description = 'Classes'
